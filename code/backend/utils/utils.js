7c020844c39749834d13bc98fcd4ea61b7437243
Eduuhms <eduardo.henriquems@gmail.com>
2025-08-02 08:52:46 -0300

    fix: resolvendo problema do padrão da data

--- code/backend/utils/utils.js
+++ code/backend/utils/utils.js
@@ -1,15 +1,18 @@
 function ehFimDeSemana(dataString) {
-  
   let data;
+  
   if (dataString.includes('/')) {
     const [dia, mes, ano] = dataString.split('/');
-    data = new Date(ano, mes - 1, dia); 
+    data = new Date(parseInt(ano), parseInt(mes) - 1, parseInt(dia));
+  } else if (dataString.includes('-')) {
+    const [ano, mes, dia] = dataString.split('-');
+    data = new Date(parseInt(ano), parseInt(mes) - 1, parseInt(dia));
   } else {
     data = new Date(dataString);
   }
   
   const diaSemana = data.getDay(); 
-  return diaSemana === 0 || diaSemana === 6; // 0 = Domingo, 6 = Sábado
+  return diaSemana === 0 || diaSemana === 6; 
 }
 
 module.exports = { ehFimDeSemana };
